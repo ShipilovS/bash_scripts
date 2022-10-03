84766ef4b901a9a444cf98ebd44dc2718345bafb
Шипилов Стефан Александрович <sshipilov@fil-it.ru>
2022-10-03 16:26:10 +0300

    adds: fix

--- task3/ptxt3.sh
+++ task3/ptxt3.sh
@@ -5,21 +5,19 @@ argArray=( "$@" )
 arrayLen="${#argArray[@]}"
 
 
-for (( i=0; i < arrayLen-1 ; i++ ))
+for (( i = 0; i < arrayLen-1 ; i++ ))
 do
-    flag=0
-    for (( j=0; j<arrayLen-i-1; j++ ))
+    for (( j = 0; j < arrayLen-i - 1; j++ ))
     do
-        # if (( ${argArray[$j]} \> ${argArray[$j+1]} )); then
-        if [ ${argArray[$j]} \> ${argArray[$j+1]} ]; then
-            temp=${argArray[$j]}
-            argArray[$j]=${argArray[$j+1]}
-            argArray[$j+1]=$temp
+        if [ ${argArray[j]} \> ${argArray[j+1]} ]; then
+            temp=${argArray[j]}
+            argArray[j]=${argArray[j+1]}
+            argArray[j+1]=$temp
         fi
     done
 done
 
-for (( l=0; l<arrayLen; l++))
+for (( l = 0; l < arrayLen; l++))
 do
-    echo "${argArray[$l]} "
+    echo "${argArray[l]} "
 done
