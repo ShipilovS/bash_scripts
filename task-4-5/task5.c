78a461926d323bf916592f6b3245ac631891fdb4
Шипилов Стефан Александрович <sshipilov@fil-it.ru>
2022-11-08 17:04:17 +0300

    Created task5.c file

---
+++ task-4-5/task5.c
@@ -0,0 +1,11 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+// для таска
+// http://grep.cs.msu.ru/givi.olnd.ru/kr2/07.html
+
+int main(int argc, char *argv[])
+{}
