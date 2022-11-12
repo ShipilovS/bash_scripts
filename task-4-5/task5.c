0ef4122250a90a4e510baca61dc6c2a4f6bcf1ba
Shipilov Stefan <stefan.shipilov@mail.ru>
2022-11-12 18:39:06 +0300

    adds: task5

--- task-4-5/task5.c
+++ task-4-5/task5.c
@@ -1,11 +1,35 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <fcntl.h>
 
-// для таска
-// http://grep.cs.msu.ru/givi.olnd.ru/kr2/07.html
+// gcc test.c -o test
+// ./task5 test test.txt
 
 int main(int argc, char *argv[])
-{}
+{
+  if (argc < 2) {
+    exit(-1);
+  }
+
+  char *filename = argv[1];
+  char *outname = argv[2];
+
+  printf("%s\n", filename);
+
+
+  pid_t pid = fork();
+  if(pid == 0){
+    int file = open(outname, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    dup2(file, 1);
+    dup2(file, 2);
+
+    execl(filename, filename, NULL);
+    // perror("execl");
+
+    close(file);
+  }
+  wait(NULL);
+}
