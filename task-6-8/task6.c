77541232575535f2497629d21a2d89189df1f621
Шипилов Стефан Александрович <sshipilov@fil-it.ru>
2022-12-02 17:54:30 +0300

    Try 1

--- task-6-8/task6.c
+++ task-6-8/task6.c
@@ -1,11 +1,11 @@
 // Создает дочерний процесс, который каждые 3 секунды посылает сигнал SIGUSR1 родительскому процессу  - есть
-// Создает в текущем каталоге именованный канал с именем reqests и открывает его для чтения - есть 
-// Создает сигнальные дескрипторы для обработки сигналов SIGINT и SIGUSR1
+// Создает в текущем каталоге именованный канал с именем requests и открывает его для чтения - есть 
+// Создает сигнальные дескрипторы для обработки сигналов SIGINT и SIGUSR1 вроде есть
 // Используя poll ожидает в цикле на дескрипторах сигналов и канала
 // При получении данных из именованного канала выводит данные на экран как текст
 // При получении сигнала SIGUSR1 выводит имя полученного сигнала на экран
 // При получении сигнала SIGINT завершает цикл ожидания
-// После завершения цикла ожидания посылает сигнал SIGTERM дочернему процессу
+// После завершения цикла ожидания посылает сигнал C дочернему процессу
 // Ждет завершение дочернего процесса и выводит на экран его код завершения
 // Удаляет ранее созданный именованный канал reqests и завершается
 
@@ -16,6 +16,27 @@
 #include <poll.h> // poll
 #include <string.h> // strsignal
 #include <fcntl.h> // open
+#include <stdlib.h>
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT){
+    printf("\nSIGINT!!!\n");
+    // exit(1);
+    raise(SIGUSR1);
+    signal(signo, SIG_DFL);
+  }
+
+  else if (signo == SIGUSR1){
+    printf("\nSignal \"%s\" has been catched\n", strsignal(signo)); // выводим название сигнала
+    // printf("Child : Received a signal from parent \n");
+    // sleep(1);
+    // kill(getppid(), SIGUSR1);
+    // exit(1);
+    signal(signo, SIG_DFL);
+  }
+
+}
 
 int main()
 {
@@ -23,14 +44,24 @@ int main()
     // создаем сигнальный файловый дескриптор для SIGALRM
     sigset_t sig;
     sigemptyset(&sig);
-
+    signal(SIGINT, sig_handler);
+    signal(SIGUSR1, sig_handler);
     sigaddset(&sig, SIGALRM);
     sigprocmask(SIG_BLOCK, &sig, NULL);
     int sigfd = signalfd(-1, &sig, 0);
 
+    sigset_t bs, os, b2;
+    sigemptyset(&bs);
+    sigaddset(&bs, SIGUSR1);
+    sigaddset(&bs, SIGUSR2);
+    sigemptyset(&b2);
+    sigaddset(&b2, SIGINT);
+    sigaddset(&b2, SIGTERM);
+    sigprocmask(0, NULL, &os);
+
     // открываем именованный канал с именем “pipe”
     // должен быть создан заранее в текущем каталоге программы командой mkfifo pipe
-    int pipefd = open("pipe", O_RDONLY);
+    int pipefd = open("request", O_RDONLY);
 
     alarm(3); // сигнал SIGALRM будет отправлен через 3 секунды
 
