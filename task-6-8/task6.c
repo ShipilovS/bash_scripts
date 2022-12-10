09148cabb3594959a91ad2da39197c33890358f3
Stefan Shipilov <stefan.shipilov@mail.ru>
2022-12-10 23:22:57 +0300

    done task6-7

--- task-6-8/task6.c
+++ task-6-8/task6.c
@@ -18,35 +18,14 @@
 #include <string.h> // strsignal
 #include <fcntl.h> // open
 #include <stdlib.h>
-volatile sig_atomic_t stop;
+volatile sig_atomic_t stop = 0;
 
-pid_t child_pid = -1;
-
-void sig_handler(int signo)
-{
-  if (signo == SIGINT){
-    printf("\nSIGINT!!!\n");
-    // exit(1);
-    raise(SIGUSR1);
-    signal(signo, SIG_DFL);
-  }
-
-  else if (signo == SIGUSR1){
-    printf("\nSignal \"%s\" has been catched\n", strsignal(signo)); // выводим название сигнала
-    // printf("Child : Received a signal from parent \n");
-    // sleep(1);
-    // kill(getppid(), SIGUSR1);
-    // exit(1);
-    signal(signo, SIG_DFL);
-  }
-
-}
+pid_t pid;
+int status = 0;
 
 void sigint_handler(int sig){
-  printf("\nCatched SIGINT \n");
+  printf("Catched SIGINT\n");
   stop = 1;
-  printf("stop = %d\n", stop);
-  // exit(0);
 }
 
 void sigusr1_handler(int sig){
@@ -54,86 +33,73 @@ void sigusr1_handler(int sig){
 }
 
 void sigterm_handler(int sig){
-  printf("SIGTERM handler");
-  printf("Child pid = %d\n", getpid());
-  kill(child_pid, SIGTERM);
+  printf("SIGTERM handler\n");
+  stop = 1;
 }
+
 int main()
 {
+    pid = fork();
+    if(pid == 0){
+        signal(SIGUSR1, SIG_IGN);
+        signal(SIGINT, SIG_IGN);
+        signal(SIGTERM, sigterm_handler);
+
+        while(!stop) {
+            kill(0, SIGUSR1);
+            sleep(3);
+        }
+        exit(0);
+    }
 
     // создаем сигнальный файловый дескриптор для SIGALRM
     sigset_t sig;
     sigemptyset(&sig);
-    signal(SIGINT, sigint_handler);
-    signal(SIGUSR1, sigusr1_handler);
-    sigaddset(&sig, SIGALRM);
+    sigaddset(&sig, SIGUSR1);
+    sigaddset(&sig, SIGINT);
+    sigaddset(&sig, SIGTERM);
     sigprocmask(SIG_BLOCK, &sig, NULL);
     int sigfd = signalfd(-1, &sig, 0);
-    int status = 0;
-
-    sigset_t bs, os, b2;
-    sigemptyset(&bs);
-    sigaddset(&bs, SIGUSR1);
-    sigaddset(&bs, SIGUSR2);
-    sigemptyset(&b2);
-    sigaddset(&b2, SIGINT);
-    sigaddset(&b2, SIGTERM);
-    sigprocmask(0, NULL, &os);
-
-    // открываем именованный канал с именем “pipe”
-    // должен быть создан заранее в текущем каталоге программы командой mkfifo pipe
     int pipefd = open("request", O_RDONLY);
 
-    alarm(3); // сигнал SIGALRM будет отправлен через 3 секунды
-
-    // подготавливаем структуру для ожидания на файловых дескрипторах
     struct pollfd pfd[] =
     {
         {.fd = sigfd, .events = POLLIN },
         {.fd = pipefd, .events = POLLIN }
     };
-    // child_pid = fork();
-    // if (child_pid == 0){
-    //   printf("Child");
-    //   // exit(1);
-    // }
-    // else {
-    //   child_pid = wait(&status);
-    // }
+
 
     while (!stop)
     {
-        // ждем данные в файловых дескрипторах
         poll(pfd, 2, -1);
-
-        // получен сигнал
         if ((pfd[0].revents & POLLIN) != 0)
         {
-            // считываем параметры сигнала
             struct signalfd_siginfo siginfo = {};
             read(sigfd, &siginfo, sizeof(siginfo));
-            // выводим имя сигнала - SIGUSR1f
-            printf("Catched signal %s\n", strsignal(siginfo.ssi_signo));
-
-            // новый сигнал SIGALRM будет отправлен через 3 секунды
-            alarm(3);
+            if(siginfo.ssi_signo == SIGINT) {
+                printf("\nCatch SIGINT with\n");
+                stop=1;
+            } 
+            else {
+                printf("Catch with pid = %d - '%s'\n", pid, strsignal(siginfo.ssi_signo));
+            }
         }
-        // есть данные в именованном канале
         if ((pfd[1].revents & POLLIN) != 0)
         {
-
-        // читаем данные из канала блоками до 16 байт и выводим на экран
             char buf[16];
             int n = read(pipefd, buf, sizeof(buf));
             if (n != 0)
             {
-                printf("%.*s", n, buf);
+                printf("Text: %.*s", n, buf);
             }
         }
     }
 
+    // close(pipefd);
+    kill(0, SIGTERM);
+    wait(&status);
+    unlink("request");
+    printf("Child  %d exit code: %d\n", getpid(), status);
 
-    raise(SIGTERM);
-
-    return 0;
+    exit(0);
 }
